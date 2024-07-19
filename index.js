f63a44f99d79bd16610599eabf02f7d72c5af6db
Rafael Terrazas <rafaelalejandroterrazas@hotmail.com>
2024-07-19 12:40:36 -0300

    Dot for decimal numbers function added Percentage Button function added Characters Limited characters to 9

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const display = document.querySelector(".display");
 let firstNumber = 0;
 let secondNumber = 0;
 let operator = "";
+let maxCharacters = 8
 
 
 function operate(firstNum, operatorType, secondNum) {
@@ -21,50 +22,58 @@ function operate(firstNum, operatorType, secondNum) {
         default:
             break;
     }
-    console.log(typeof secondNumOperate);
 
 }
+
+
 
 // Listener handler for every button
 buttons.forEach(element => element.addEventListener("click", (event) => {
     if (event.target.classList.contains("aNumber") && operator === "") {
         // To make operations from the beginning
-        if (firstNumber == 0) {
+        if (firstNumber === 0 || firstNumber === "0") {
             firstNumber = event.target.textContent;
         // Normal Operations (without starting with 0)
         } else {
             firstNumber += event.target.textContent;
-
         }
+        if (firstNumber.length > maxCharacters) {
+            firstNumber = firstNumber.substring(0, maxCharacters);
+        } 
+
         display.textContent = firstNumber;
     }
-
-    else if (event.target.classList.contains("operator") && secondNumber === 0) {
+    // Select the Operator
+    else if (event.target.classList.contains("operator") && secondNumber == 0) {
         operator = event.target.textContent;
     }
     // To do multiple operations in sequence without pressing Equals
-    else if (event.target.classList.contains("operator") && secondNumber !== 0) {
-        display.textContent = operate(firstNumber, operator, secondNumber);
-        firstNumber = operate(firstNumber, operator, secondNumber);
+    else if (event.target.classList.contains("operator") && secondNumber != 0) {
+        display.textContent = operate(firstNumber, operator, secondNumber).toString().substring(0,maxCharacters);
+        firstNumber = operate(firstNumber, operator, secondNumber).toString().substring(0, maxCharacters);
         secondNumber = 0;
         operator = event.target.textContent;
     }
-
+    // Select a second number
     else if(event.target.classList.contains("aNumber") && operator !== "") {
-        if (secondNumber == 0) {
+        if (secondNumber === 0 || secondNumber === "0") {
         secondNumber = event.target.textContent;
     } else {
         secondNumber += event.target.textContent;
     }
+    if (secondNumber.length > maxCharacters){
+        secondNumber = secondNumber.substring(0, maxCharacters);
+    }
     display.textContent = secondNumber;
     }
-
+    // Equals button
     else if(event.target.textContent === "=" && operator !== ""){
-        display.textContent = operate(firstNumber, operator, secondNumber);
-        firstNumber = operate(firstNumber, operator, secondNumber);
+        display.textContent = operate(firstNumber, operator, secondNumber).toString().substring(0, maxCharacters);
+        firstNumber = operate(firstNumber, operator, secondNumber).toString().substring(0, maxCharacters);
         secondNumber = 0;
         operator = "";
     }
+    // Clear Button
     else if(event.target.textContent === "Clear"){
         firstNumber = 0;
         secondNumber = 0;
@@ -72,6 +81,33 @@ buttons.forEach(element => element.addEventListener("click", (event) => {
         display.textContent = firstNumber;
 
     }
+    
+    else if(event.target.textContent === ".") {
+        firstNumber = firstNumber.toString();
+        secondNumber = secondNumber.toString();
+        if(!firstNumber.includes(".") && operator === "") {
+            firstNumber = (firstNumber + ".");
+            display.textContent = firstNumber;
+        }
+        else if(!secondNumber.includes(".") && operator !== "") {
+            secondNumber = (secondNumber + ".");
+            display.textContent = secondNumber;
+        }
+
+
+    }
+    
+    else if(event.target.textContent === "%"){
+        if(firstNumber != 0 && secondNumber == 0){
+            firstNumber = getPercentage(firstNumber)
+            display.textContent = firstNumber;
+        }
+        else if(secondNumber != 0){
+            secondNumber = getPercentage(secondNumber);
+            display.textContent = secondNumber;
+        }
+    }
+
 
 
 }
@@ -91,5 +127,8 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+function getPercentage(a) {
+    return a / 100;
 }
 
